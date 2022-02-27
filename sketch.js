88dbbc22eaf63f5b997589b120ba0f9cf5931496
saxophonebug <ethan.andrew.cobb@gmail.com>
2022-02-27 15:31:45 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  textureGraphic = createGraphics(400, 400);
+  textureGraphic.background(0, 11, 19);
+  textureGraphic.textSize(400);
+  textureGraphic.text('😡', -75, 340);
+  
+  /*
+    frameRate(30);
+    createLoop({duration:5, gif:true}); 
+  */
 }
 
 function draw() {
-  background(220);
+  let tick = millis() / 350;
+  background(1, 11, 19);
+
+  translate(sin(tick / 2) * (width / 3), cos(tick / 3) * (height / 3), -height / 2);
+  rotateZ(tick / 2);
+  rotateY(tick);
+  texture(textureGraphic);
+
+  box(height / 2);
 }
